600f725ac7b00dfe1eb7ad8f50214b1bf21cc8e4
sentilab <sentilab@gmail.com>
2018-12-07 22:48:26 +0900

    first commit

---
+++ app/src/main/java/com/kplo/samplesenti/ChatActivity.java
@@ -0,0 +1,26 @@
+package com.kplo.samplesenti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!123");
+
+        //1. recyclerView - 반복
+        //2. 디비 내용을 넣는다
+        //3. 상대방폰에 채팅 내용이 보임
+    }
+}
