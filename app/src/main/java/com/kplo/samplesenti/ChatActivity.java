041a0344a912d3c35031b7946191cf93e4d1e578
sentilab <sentilab@gmail.com>
2018-12-08 21:05:49 +0900

    [센치한 개발자] (19) 안드로이드 스튜디오 기초 강의 : 채팅 앱 만들기 - 2 : Firebase, Realtime Database 소스 커밋

--- app/src/main/java/com/kplo/samplesenti/ChatActivity.java
+++ app/src/main/java/com/kplo/samplesenti/ChatActivity.java
@@ -1,8 +1,13 @@
 package com.kplo.samplesenti;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,9 +23,46 @@ public class ChatActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!123");
+        myRef.setValue("Hello, World!123");
+        myRef.setValue("Hello, World!123");
+        myRef.setValue("Hello, World!123");
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         //1. recyclerView - 반복
         //2. 디비 내용을 넣는다
-        //3. 상대방폰에 채팅 내용이 보임
+        //3. 상대방폰에 채팅 내용이 보임 - get
+
+        //1-1. recyclerview - chat data
+        //1. message, nickname - Data Transfer Object
+
+
+
+
     }
 }
