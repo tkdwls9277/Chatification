a6cf8f4a8f694ba6afb1dc45cae6c7c0ee75809f
tkdwl <tkdwls9277>
2019-12-10 09:35:16 +0900

    Merge branch 'new_branch'...?

--- app/src/main/java/com/example/chatification/MainActivity.java
+++ app/src/main/java/com/example/chatification/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.chatification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editId;
+    private EditText editPw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editId = findViewById(R.id.editId);
+        editPw = findViewById(R.id.editPw);
+
     }
 }
---
+++ app/src/main/java/com/example/chatification/SignUpActivity.java
@@ -0,0 +1,36 @@
+package com.example.chatification;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText sEditId;
+    private EditText sEditPw;
+    private EditText sEditName;
+    private EditText sEditAge;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        sEditId = findViewById(R.id.sEditId);
+        sEditPw = findViewById(R.id.sEditPw);
+        sEditName = findViewById(R.id.sEditName);
+        sEditAge = findViewById(R.id.sEditAge);
+
+    }
+
+    public void sOnClickBtn(View v) {
+        switch (v.getId()) {
+            case R.id.confirmBtn:
+                break;
+            case R.id.cancelBtn:
+                break;
+        }
+    }
+}
